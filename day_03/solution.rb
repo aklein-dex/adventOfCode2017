13636a82f9ce9281bdcaadcdcd39c7c19fee5ab5
Alex <alexandre.klein@tanukisoftware.com>
2017-12-08 18:03:49 +0900

    working on part 2

--- day_03/solution.rb
+++ day_03/solution.rb
@@ -2,40 +2,74 @@
 
 puts "--- Day 3: Spiral Memory ---"
 
-if ARGV[0] == "2"
-    puts "--- Part 2:"
-    
-else
-    puts "--- Part 1:"
-    
-end
-
 # My input for this challenge
 INPUT = 312051
 
-# Formula to find the bottom right corner of a layer: (2n + 1)^2
-layer = 1
-#bottom right corner
-corner = 9
-while corner < INPUT
-  corner = (2 * layer + 1) ** 2
-  layer = layer + 1
+def part2(input)
+  data = Hash.new
+  data[0] = Hash.new
+  data[0][0] = 1
+  result = 1
+  
+  x = 0
+  y = 0
+  layer = 0
+  
+  while result < input
+    
+    data[x] = Hash.new if !data.has_key?(x)
+    
+    result =  dataValue(data, x-1, y+1) + dataValue(data, x, y+1) + dataValue(data, x+1, y+1) +
+              dataValue(data, x-1, y)   +                           dataValue(data, x+1, y)   +
+              dataValue(data, x-1, y-1) + dataValue(data, x, y-1) + dataValue(data, x+1, y-1)
+    
+    data[x][y] = result
+  end
+  
+  return result
 end
 
-puts "  Layer: #{layer}"
-puts "  Corner: #{corner}"
+def dataValue(data, x, y)
+  if data.has_key?(x) and data[x].has_key?(y)
+    return data[x][y]
+  else
+    return 0
+  end
+end
 
-# This is the length of the square's side
-side_length =  Math.sqrt(corner)
-puts "  Square's side: #{side_length}"
+def part1(input)
+  # Formula to find the bottom right corner of a layer: (2n + 1)^2
+  layer = 1
+  #bottom right corner
+  corner = 9
+  while corner < input
+    corner = (2 * layer + 1) ** 2
+    layer = layer + 1
+  end
 
-# Have to find how far away from a corner the INPUT is located
-distance = INPUT % side_length
-puts "  Distance: #{distance}"
+  puts "  Layer: #{layer}"
+  puts "  Corner: #{corner}"
 
-# Number of steps: half of the side length vertically + half of the side length horizontally - distance
-# which is: (side_length/2) + (side_length/2) - distance
-result = side_length - distance
+  # This is the length of the square's side
+  side_length =  Math.sqrt(corner)
+  puts "  Square's side: #{side_length}"
 
-puts result
+  # Have to find how far away from a corner the input is located
+  distance = input % side_length
+  puts "  Distance: #{distance}"
 
+  # Number of steps: half of the side length vertically + half of the side length horizontally - distance
+  # which is: (side_length/2) + (side_length/2) - distance
+  return side_length - distance
+end
+
+if ARGV[0] == "2"
+    puts "--- Part 2:"
+    result = part2(INPUT)
+else
+    puts "--- Part 1:"
+    result = part1(INPUT)
+end
+
+
+puts "Result: #{result}"
