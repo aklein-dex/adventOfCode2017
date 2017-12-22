5ceec2cf0f13c8dfafa069728516099540774515
Alex <alexandre.klein@tanukisoftware.com>
2017-12-22 15:28:24 +0900

    add day 13

---
+++ day_13/solution.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby
+
+puts "--- Day 13: Packet Scanners ---"
+
+def getScannerPosition(depth, pico)
+  # Length of a cycle before the scanner comes back to position 0
+  cycle = (2*depth) - 2
+  
+  # Position of the scanner
+  position = pico % cycle
+  
+  # If true, then the scanner is on his way back to position 0
+  if position >= depth
+    position = (depth-1) - (position - (depth-1))
+  end
+  
+  return position
+end
+
+# Firewall rules
+firewall = Array.new
+
+# Parse the file
+input = File.open('input.txt', 'r')
+input.each_line do |line|
+  layer = line.chomp.split(": ")
+  firewall[layer[0].to_i] = layer[1].to_i
+end
+input.close
+
+# Send the packet after a delay of "pico" picoseconds.
+# If strict is true then it will return -1 if get caught.
+# If strict is false, then it will continue to run and return the severity.
+def sendPacket(firewall, pico, strict)
+  result = 0
+  
+  firewall.each_with_index do |depth, layer|
+    if depth == nil
+      pico+=1
+      next
+    end
+    
+    position = getScannerPosition(depth, pico)
+    
+    if position == 0
+      return -1 if strict
+      result+=depth*layer 
+    end
+
+    pico+=1
+  end
+  
+  return result
+end
+
+# Find the number of picoseconds to delay
+def part2(firewall)
+  delay = 10
+  
+  while delay < 5000000
+    result = sendPacket(firewall, delay, true)
+    return delay if result == 0
+    delay+=1
+  end
+  
+  puts "reach max delay #{delay}"
+  return -1
+end
+
+# Find the severity when being caught
+def part1(firewall)
+  return sendPacket(firewall, 0, false)
+end
+
+puts "Part 1: #{part1(firewall)}"
+puts "Part 2: #{part2(firewall)}"
